7d6d2bd782c8b7b3a2e4fe65166948650ccf487a
YMGYM <good_day1234@naver.com>
2020-09-01 21:38:43 +0900

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'room#main'
+  devise_for :users
+  root 'rooms#main'
   resources :rooms
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
