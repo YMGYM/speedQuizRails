dbb194b9c4a8f2508c87bff88de85f1477858d81
idea7654 <idea7654@naver.com>
2020-10-02 20:17:35 +0900

    update game

--- server.js
+++ server.js
@@ -16,17 +16,8 @@ var index = 0;
 var answer = [];
 var url = [];
 var startTime = [];
+var question;
 
-fs.createReadStream('./Questions/girlsIdol.csv')
-  .pipe(csv())
-  .on('data', (row) => {
-    answer.push({answer: row.answer, keyword: row.answerKeyword});
-    url.push(row.src);
-    startTime.push(row.startTime);
-  })
-  .on('end', () => {
-    console.log('CSV file successfully processed');
-  });
 /*
 client.keys('*', (err, keys) => {
   for (const [key, value] of Object.entries(keys)) {
@@ -41,26 +32,38 @@ client.keys('*', (err, keys) => {
 io.sockets.on("connection", function(socket) {
   var score = 0;
 
-
-  io.emit('answer', {
-    url: url,
-    startTime: startTime
+  socket.on('question', (data) => {
+    switch(data){
+      case "인싸 감성! 당신의 K-POP 걸그룹  지식은?(난이도 상)":
+        question = './Questions/girlsIdol.csv';
+        break;
+      case "추억을 불러 일으키는 게임들.. (난이도 중)":
+        question = './Questions/games.csv';
+        break;
+      case "아는듯 모르는... 긴가민가한 광고들 (난이도 하)":
+        question = './Questions/advertise.csv';
+        break;
+      case "당신이 최고의 오타쿠! 일본 애니메이션 OST들 (난이도 중)":
+        question = './Questions/animesong.csv';
+        break;
+    }
+    fs.createReadStream(question)
+      .pipe(csv())
+      .on('data', (row) => {
+        answer.push({answer: row.answer, keyword: row.answerKeyword});
+        url.push(row.src);
+        startTime.push(row.startTime);
+      })
+      .on('end', () => {
+        console.log('CSV file successfully processed');
+        io.emit('answer', {
+          url: url,
+          startTime: startTime
+        });
+      });
   });
+
   socket.on("sendUser", (data) => {
-    //redisdb와 data.value비교,
-    //만약 맞으면 score추가, 다시 보냄
-    /*
-    client.hmset('chat', 'email', data.email, 'chat', data.value);
-    client.hgetall('chat', async(err, obj) => {
-      if(kotae === obj.chat){
-        //현재 정답은 '밀리시타최고'로 등록해놓음.
-        console.log('정답입니다');
-        flag = await true;
-        //이거 대신에 스코어가 올라가도 되고 맞췄을 때 보상은 생각해둘 것.
-      }else{
-        flag = await false;
-      }
-    });*/
     index = data.i;
     console.log(index);
     if(answer[index].answer === data.value || answer[index].keyword === data.value){
