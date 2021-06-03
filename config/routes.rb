00f2bf4aa593fc7bd44396420b690eea8aac1542
idea7654 <idea7654@naver.com>
2021-06-03 17:04:57 +0900

    add japanese

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
   root 'rooms#main'
-  resources :rooms
+  scope '(:locale)', locale: /#{I18n.available_locales.map(&:to_s).join('|')}/ do
+    resources :rooms
+    devise_for :users
 
-  get '/secret/:id' => 'rooms#secret'
-  post '/passcheck/:id' => 'rooms#passCheck'
-  post '/search' => 'rooms#search'
+    get '/secret/:id' => 'rooms#secret'
+    post '/passcheck/:id' => 'rooms#passCheck'
+    post '/search' => 'rooms#search'
 
-  delete '/roomquit' => 'rooms#roomquit'
+    delete '/roomquit' => 'rooms#roomquit'
 
-  get '/chat' => 'chat#index'
-  get 'help' => 'rooms#help', as: :help
-  post '/startGame' => 'rooms#startGame', as: :startGame
+    get '/chat' => 'chat#index'
+    get 'help' => 'rooms#help', as: :help
+    post '/startGame' => 'rooms#startGame', as: :startGame
 
-  get '/result/:id/:winner_id' => 'rooms#result', as: :result
+    get '/result/:id/:winner_id' => 'rooms#result', as: :result
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
